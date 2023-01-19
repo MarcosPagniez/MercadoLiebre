16e450f5136d234045e2248494eabaef758ea0bd
MarcosPagniez <marcos.pagniez@hotmial.com>
2023-01-19 20:22:11 -0300

    solucion

--- app-express/app.js
+++ app-express/app.js
@@ -1,11 +1,20 @@
 const express = require ("express");
-
+const path=require("path");
 const port =process.env.PORT || 3001;
 
 const app = express();
-
+const publicfolder=path.resolve("public");
+app.use(express.static(path.join(__dirname, 'public')))
 app.listen(port,() => console.log(`Servidor escuchando en puerto ${port}`));
 
 app.get('/',(req,res)=> {
-    res.sendFile(path.join(__dirname, "/app-express/views/home.html"));
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+})
+
+app.get('/login',(req,res)=> {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+})
+
+app.get('/register',(req,res)=> {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
 })
