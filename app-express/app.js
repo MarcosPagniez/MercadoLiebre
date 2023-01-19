6967be94bba03f5d66d4828d4d528375fdb2d61b
MarcosPagniez <marcos.pagniez@hotmial.com>
2023-01-19 19:24:00 -0300

    agrego home

--- app-express/app.js
+++ app-express/app.js
@@ -5,3 +5,7 @@ const port =process.env.PORT || 3001;
 const app = express();
 
 app.listen(port,() => console.log(`Servidor escuchando en puerto ${port}`));
+
+Router.get(`/`,(req,res)=> {
+    res.send("/home")
+})
